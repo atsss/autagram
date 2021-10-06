4bb695a93573a25e35b2c3d606304fbfd566df76
atsss <atsushi0623itoh@gmail.com>
2021-10-06 12:34:38 +0900

    small tweak

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from config.targets import targets
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 def init():
-    Bot(driver, credentials[0]['name'], credentials[0]['password'], targets[:2], message, False)
+    Bot(driver, credentials[0]['name'], credentials[0]['password'], targets[:10], message, False)
     print('DONE: {0}'.format(credentials[0]['name']))
 
-    Bot(driver, credentials[1]['name'], credentials[1]['password'], targets[2:4], message)
+    Bot(driver, credentials[1]['name'], credentials[1]['password'], targets[10:20], message)
     print('DONE: {0}'.format(credentials[1]['name']))
 
-    # Bot(driver, credentials[2]['name'], credentials[2]['password'], targets[4:], message)
-    # print('DONE: {0}'.format(credentials[2]['name']))
+    Bot(driver, credentials[2]['name'], credentials[2]['password'], targets[20:], message)
+    print('DONE: {0}'.format(credentials[2]['name']))
 
     driver.quit()
 
